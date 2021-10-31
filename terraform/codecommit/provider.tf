efb5ae33ba30da30411913384a234b75834a6ec8
Danny <danny@Dannys-MacBook-Air.local>
2021-10-31 14:26:50 -0400

    tf fmt changes

--- terraform/codecommit/provider.tf
+++ terraform/codecommit/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dgimeno-repos"
-    key    = "codecommit/s3/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "dgimeno-repos"
+    key     = "codecommit/s3/terraform.tfstate"
+    region  = "us-east-1"
     profile = "codecommit"
   }
   required_providers {
